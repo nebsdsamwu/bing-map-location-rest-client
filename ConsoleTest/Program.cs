fd05cfedbec579ffccf55e5b847580c8abc58a07
nebsdsamwu <sam.s.wu@newegg.com>
2016-08-12 17:12:12 -0700

    first commit

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using LocationServiceClient;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string endpoint = ConfigurationManager.AppSettings["locEndPoint"];
+            string devKey = ConfigurationManager.AppSettings["bingMapDevKey"];
+
+            Console.ReadKey();
+        }
+    }
+}
